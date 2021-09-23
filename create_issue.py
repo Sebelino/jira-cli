3ad196a5ddb48e2aba0225df81ef0b888ede87db
Sebastian Olsson <sebelino7@gmail.com>
2021-09-23 11:41:00 +0200

    refactor: Move method into class

--- create_issue.py
+++ create_issue.py
@@ -4,51 +4,54 @@ import json
 from atlassian import Jira
 from pprint import pprint
 
-def create_issue(project_key: str):
-    board, = jira.get_all_agile_boards(project_key=project_key)["values"]
+class IssueCreator:
 
-    all_sprints = jira.get_all_sprint(board["id"])
+    def __init__(self):
+        self.project_key = "EP"
 
-    current_sprint, = [s for s in all_sprints["values"] if s["state"] == "active"]
+    def create(self):
+        board, = jira.get_all_agile_boards(project_key=self.project_key)["values"]
 
-    fields = {
-        "summary": "(delet this) Test issue",
-        "project": {
-            "key": project_key,
-        },
-        "issuetype": {
-            "name": "Task",
-        },
-        "customfield_10019": current_sprint["id"],
-    }
+        all_sprints = jira.get_all_sprint(board["id"])
 
-    created_issue = jira.create_issue(fields)
+        current_sprint, = [s for s in all_sprints["values"] if s["state"] == "active"]
 
-    labels = jira.get_issue_labels(created_issue['id'])
+        fields = {
+            "summary": "(delet this) Test issue",
+            "project": {
+                "key": self.project_key,
+            },
+            "issuetype": {
+                "name": "Task",
+            },
+            "customfield_10019": current_sprint["id"],
+        }
 
-    if "to-be-groomed" in labels:
-        labels.remove("to-be-groomed")
+        created_issue = jira.create_issue(fields)
 
-    fields = {
-        "labels": labels,
-    }
+        labels = jira.get_issue_labels(created_issue['id'])
 
-    jira.update_issue_field(created_issue['key'], fields)
+        if "to-be-groomed" in labels:
+            labels.remove("to-be-groomed")
 
-    all_users = [u for u in jira.get_all_assignable_users_for_project(project['key'], limit=1000)]
+        fields = {
+            "labels": labels,
+        }
 
-    if len(all_users) >= 1000:
-        raise Exception("This script currently assumes < 1000 Jira users")
+        jira.update_issue_field(created_issue['key'], fields)
 
-    me, = [u for u in all_users if 'Sebastian Olsson' in u['displayName']]
+        all_users = [u for u in jira.get_all_assignable_users_for_project(self.project_key, limit=1000)]
 
-    my_accountid = me['accountId']
+        if len(all_users) >= 1000:
+            raise Exception("This script currently assumes < 1000 Jira users")
 
-    jira.assign_issue(created_issue['key'], account_id=my_accountid)
+        me, = [u for u in all_users if 'Sebastian Olsson' in u['displayName']]
+
+        my_accountid = me['accountId']
+
+        jira.assign_issue(created_issue['key'], account_id=my_accountid)
 
 if __name__ == "__main__":
-
-    project_key = "EP"
 
     with open("credentials.json", 'r') as f:
         credentials = json.load(f)
@@ -59,10 +62,6 @@ if __name__ == "__main__":
         password=credentials["api_token"],
     )
 
-    project = jira.project(project_key)
+    creator = IssueCreator()
 
-    issue_types = project['issueTypes']
-
-    task_issue_type, = [tpe for tpe in project['issueTypes'] if tpe["name"] == "Task"]
-
-    #create_issue(project)
+    creator.create()
