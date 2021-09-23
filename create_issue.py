d002bba89ce9dcb2541989d2343ca5fcae961274
Sebastian Olsson <sebelino7@gmail.com>
2021-09-23 12:23:18 +0200

    refactor: Move jira into class

--- create_issue.py
+++ create_issue.py
@@ -11,14 +11,23 @@ class IssueCreator:
     def __init__(self):
         self.project_key = "EP"
 
+        with open("credentials.json", 'r') as f:
+            credentials = json.load(f)
+
+        self.jira = Jira(
+            url=credentials["url"],
+            username=credentials["username"],
+            password=credentials["api_token"],
+        )
+
     def create(self):
         issue = self.create_issue()
         self.postprocess_issue(issue)
 
     def create_issue(self):
-        board, = jira.get_all_agile_boards(project_key=self.project_key)["values"]
+        board, = self.jira.get_all_agile_boards(project_key=self.project_key)["values"]
 
-        all_sprints = jira.get_all_sprint(board["id"])
+        all_sprints = self.jira.get_all_sprint(board["id"])
 
         current_sprint, = [s for s in all_sprints["values"] if s["state"] == "active"]
 
@@ -33,23 +42,23 @@ class IssueCreator:
             "customfield_10019": current_sprint["id"],
         }
 
-        return jira.create_issue(fields)
+        return self.jira.create_issue(fields)
 
     def postprocess_issue(self, issue):
         self.remove_labels(issue)
         self.assign(issue)
 
     def remove_labels(self, issue):
-        labels = jira.get_issue_labels(issue['id'])
+        labels = self.jira.get_issue_labels(issue['id'])
         if "to-be-groomed" in labels:
             labels.remove("to-be-groomed")
         fields = {
             "labels": labels,
         }
-        jira.update_issue_field(issue['key'], fields)
+        self.jira.update_issue_field(issue['key'], fields)
 
     def assign(self, issue):
-        all_users = [u for u in jira.get_all_assignable_users_for_project(self.project_key, limit=self.USER_LIMIT)]
+        all_users = [u for u in self.jira.get_all_assignable_users_for_project(self.project_key, limit=self.USER_LIMIT)]
 
         if len(all_users) >= self.USER_LIMIT:
             raise Exception("This script currently assumes < 1000 Jira users")
@@ -58,19 +67,8 @@ class IssueCreator:
 
         my_accountid = me['accountId']
 
-        jira.assign_issue(issue['key'], account_id=my_accountid)
+        self.jira.assign_issue(issue['key'], account_id=my_accountid)
 
 if __name__ == "__main__":
-
-    with open("credentials.json", 'r') as f:
-        credentials = json.load(f)
-
-    jira = Jira(
-        url=credentials["url"],
-        username=credentials["username"],
-        password=credentials["api_token"],
-    )
-
     creator = IssueCreator()
-
     creator.create()
